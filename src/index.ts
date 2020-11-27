45d50cc0215a9f1497eef7b04ae1c40571effc70
Pon Cheol, Ku (구본철) <bbon@bbon.kr>
2020-11-27 21:38:16 +0900

    Add npmignore

--- src/index.ts
+++ src/index.ts
@@ -8,76 +8,36 @@ const app = new Command();
 
 const checker = new Command('check');
 
-checker
-    .arguments('check')
-    // .option('-s, --source <source>', 'Source location', '.')
-    // .option(
-    //     '-t, --type <type>',
-    //     'Use Typescript (tsx) or javascript (jsx)',
-    //     'typescript',
-    // )
-    // .option('-f, --force', 'Overwrite file')
-    .action((args) => {
-        const { source, force, typescript } = app;
+checker.arguments('check').action((args) => {
+    const { source, force, typescript } = app;
 
-        const check = new ListAction({
-            source: source,
-            force: force,
-            useTypescript: Boolean(typescript),
-        });
-        check.invoke();
+    const check = new ListAction({
+        source: source,
+        force: force,
+        useTypescript: Boolean(typescript),
     });
-// // .parse(process.argv);
+    check.invoke();
+});
 
 const writer = new Command('write');
-writer
-    .arguments('write')
-    // .option('-s, --source <source>', 'Source location')
-    // .option('-t, --type <type>', 'Use Typescript (tsx) or javascript (jsx)')
-    // .option('-f, --force', 'Overwrite file')
-    .action((args) => {
-        const { source, force, typescript } = app;
+writer.arguments('write').action((args) => {
+    const { source, force, typescript } = app;
 
-        const writer = new WriteAction({
-            source: source,
-            force: force,
-            useTypescript: Boolean(typescript),
-        });
-        writer.invoke();
+    const writer = new WriteAction({
+        source: source,
+        force: force,
+        useTypescript: Boolean(typescript),
     });
-// // .parse(process.argv);
+    writer.invoke();
+});
 
 app.version('v1.0.0', '-v, --version', 'Display version')
     .helpOption('-h, --help', 'Display help')
-    .option('-s, --source <source>', 'Source location')
+    .option('-s, --source <source>', 'Source location', '.')
     .option('-t, --typescript', 'Use Typescript (tsx)')
     .option('-f, --force', 'Overwrite file')
     .addCommand(checker, { isDefault: true })
     .addCommand(writer)
-    // .command('check <check>')
-    // .alias('c')
-    // .action((args) => {
-    //     console.info('args: ', args);
-    //     const { source, force, type } = app;
-    //     const check = new Check({
-    //         source: source,
-    //         force: force,
-    //         useTypescript:
-    //             typeof type === 'string' && type.toLowerCase() === 'typescript',
-    //     });
-    //     check.run();
-    // })
-    // .command('write <write>')
-    // .alias('w')
-    // .action((args) => {
-    //     const { source, force, type } = app;
-    //     const writer = new Writer({
-    //         source: source,
-    //         force: force,
-    //         useTypescript:
-    //             typeof type === 'string' && type.toLowerCase() === 'typescript',
-    //     });
-    //     writer.run();
-    // })
-
     .parse(process.argv);
+
+export default app;
